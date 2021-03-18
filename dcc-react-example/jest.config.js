ff0784928e420df41082e9e2d35d50b2904dec09
Moni Ali <munsoor.ali@accenturefederal.com>
2021-03-18 13:59:22 -0400

    scaffolding

---
+++ dcc-react-example/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  projects: [
+    '<rootDir>/apps/webapp',
+    '<rootDir>/apps/mobileapp',
+    '<rootDir>/libs/api',
+    '<rootDir>/libs/ui-lib',
+  ],
+};
---
+++ dcc-react-example/libs/ui-lib/src/lib/ui-lib.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './ui-lib.module.css';
+
+/* eslint-disable-next-line */
+export interface UiLibProps {}
+
+export function UiLib(props: UiLibProps) {
+  return (
+    <div>
+      <h1>Welcome to ui-lib!</h1>
+    </div>
+  );
+}
+
+export default UiLib;
